e50fd354b603c9035f099eb281309615158ede03
charlessommers <csommers@udel.edu>
2025-03-01 15:13:05 -0500

    Added Game class and merged python files

--- server.py
+++ server.py
@@ -1,10 +1,36 @@
-from bottle import route, run, template
+from bottle import route, run
+from emails import Email
+from random import shuffle
+from dataclasses import dataclass
+
+@dataclass
+class Email:
+    name: str
+    domain: str
+    subject: str
+    body: str
+
+class Game:
+    day: int = 1
+    emails: list[Email] = []
+    times_phished: int = 0
+
+    @classmethod
+    def generate_emails(cls) -> None:
+        ''' Fills the emails list with random emails '''
+        cls.emails.clear()
+        for _ in range(cls.day): cls.emails.append(cls.make_email())
+        shuffle(cls.emails)
+    
+    @classmethod
+    def make_email(cls) -> Email: ''' Placeholder for function to return an email. '''
+
 
 @route('/')
 def index() -> str: return hello('homeslice@phishmail.com')
 
 @route('/hello/<name>')
 def hello(name):
-    return template(f'<b>Hello {name}</b>!')
+    return f'Hello {name}!'
 
-run(host='localhost', port=8888, debug=True)
+run(host='localhost', port=8080, debug=True)
